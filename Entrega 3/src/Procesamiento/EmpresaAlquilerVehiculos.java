35060b954ae3c806377adc56eb1dd51c8dab941b
aarevalom0 <a.arevalom@uniandes.edu.co>
2023-10-13 15:53:18 -0500

    start otro

--- Entrega 3/src/Procesamiento/EmpresaAlquilerVehiculos.java
+++ Entrega 3/src/Procesamiento/EmpresaAlquilerVehiculos.java
@@ -1,10 +1,12 @@
 package Procesamiento;
 import java.util.*;
+import Modelo.Seguro;
 import Modelo.CategoriaVehiculo;
 import Modelo.Empleado;
 public class EmpresaAlquilerVehiculos {
 	private HashMap<String, CategoriaVehiculo> categorias = new HashMap<String, CategoriaVehiculo> ();
 	private HashMap<String, Empleado> empleados = new HashMap<String, Empleado> ();
+	private ArrayList<Seguro> seguros = new ArrayList<Seguro>;
 	
 
 }
